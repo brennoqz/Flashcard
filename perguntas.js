511526e8c14ae944fd727c025769dff8e4a9b13f
brennoqz <silva.santiago.breno@escola.pr.gov.br>
2025-06-05 13:12:55 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Pareamento na trocação ff',
+    'Ryan, Mandela ou Themoans',
+    'Themoans vulgo'
+)
+
+criaCartao(
+    'Pergunta sobre gamesense',
+    'Espera para atirar ou atira primeiro?',
+    'Ameaça a atirar primeiro e espera ele atirar primeiro, depois que atirou da 3 capa'
+)
+
+criaCartao(
+    'Ump roxa',
+    'Qual o atributo da ump roxa',
+    'Ela sobe 2 pontos na velocidade de disparo, ajudano a matar mai rapido'
+)
+
+criaCartao(
+    'Movimentação',
+    'Tela parada é coisa de gente?',
+    'Não'
+)
+
+criaCartao(
+    'Regedit',
+    'Regedit é hack?',
+    'Nao, só piora a sensi essa nojeira'
+)
